bd8d0fd8de4fe5b209cf368600ea189bf89dbf7b
Codiopath <63225166+nehap27@users.noreply.github.com>
2020-07-20 18:26:47 +0530

    Feature/basic setup text ui (#1)
    Create basic project
    Explore Text component and add customizations

---
+++ SwiftUIStarter/SwiftUIStarter/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  SwiftUIStarter
+//
+//  Created by Pujary, Neha on 20/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10.0, content: {
+            Text("Hey, Sherlock!")
+                .font(.title)
+                .fontWeight(.medium)
+                .padding(8)
+                .background(Color.orange)
+                .shadow(radius: 3)
+                .cornerRadius(8)
+                .alignmentGuide(HorizontalAlignment.center) { d in
+                    return d[HorizontalAlignment.trailing] - 5.0
+            }
+            Text("Let's solve some mysteries...")
+                .italic()
+                .font(.system(size: 25))
+                .foregroundColor(Color("subtitleColor"))
+                .strikethrough(true, color: Color("strikethroughColor"))
+                .padding(5)
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
